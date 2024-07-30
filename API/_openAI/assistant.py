6b65c37db24e8c0bf1718e6607c0dbd1437a42f4
Timmyy <Timmyy@TIMMYS>
2024-07-30 14:39:52 +0500

    feat: added function calling in assistant.py

--- API/_openAI/assistant.py
+++ API/_openAI/assistant.py
@@ -1,4 +1,5 @@
 import os
+import json
 from openai import OpenAI
 import tiktoken
 from dotenv import load_dotenv
@@ -10,7 +11,7 @@ _MODEL = "gpt-4o-mini"
 
 load_dotenv()
 client = OpenAI()
-encoding = tiktoken.encoding_for_model(_MODEL)
+# encoding = tiktoken.encoding_for_model(_MODEL)
 
 
 def get_song_title(n):
@@ -47,31 +48,66 @@ def get_song_title(n):
     return songs[n % len(songs)]
 
 
+# class EventHandler(AssistantEventHandler):
+#     @override
+#     def on_text_created(self, text) -> None:
+#         print(f"\nassistant > ", end="", flush=True)
+
+#     @override
+#     def on_text_delta(self, delta, snapshot):
+#         print(delta.value, end="", flush=True)
+
+#     def on_tool_call_created(self, tool_call):
+#         print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+#     def on_tool_call_delta(self, delta, snapshot):
+#         if delta.type == "code_interpreter":
+#             if delta.code_interpreter.input:
+#                 print(delta.code_interpreter.input, end="", flush=True)
+#             if delta.code_interpreter.outputs:
+#                 print(f"\n\noutput >", flush=True)
+#                 for output in delta.code_interpreter.outputs:
+#                     if output.type == "logs":
+#                         print(f"\n{output.logs}", flush=True)
+
+
 class EventHandler(AssistantEventHandler):
     @override
-    def on_text_created(self, text) -> None:
-        print(f"\nassistant > ", end="", flush=True)
+    def on_event(self, event):
+        # Retrieve events that are denoted with 'requires_action'
+        # since these will have our tool_calls
+        if event.event == "thread.run.requires_action":
+            run_id = event.data.id  # Retrieve the run ID from the event data
+            self.handle_requires_action(event.data, run_id)
 
-    @override
-    def on_text_delta(self, delta, snapshot):
-        print(delta.value, end="", flush=True)
+    def handle_requires_action(self, data, run_id):
+        tool_outputs = []
 
-    def on_tool_call_created(self, tool_call):
-        print(f"\nassistant > {tool_call.type}\n", flush=True)
+        for tool in data.required_action.submit_tool_outputs.tool_calls:
+            if tool.function.name == "get_song_title":
+                args = json.loads(tool.function.arguments)
+                song_title = get_song_title(args["songnum"])
+                tool_outputs.append({"tool_call_id": tool.id, "output": song_title})
 
-    def on_tool_call_delta(self, delta, snapshot):
-        if delta.type == "code_interpreter":
-            if delta.code_interpreter.input:
-                print(delta.code_interpreter.input, end="", flush=True)
-            if delta.code_interpreter.outputs:
-                print(f"\n\noutput >", flush=True)
-                for output in delta.code_interpreter.outputs:
-                    if output.type == "logs":
-                        print(f"\n{output.logs}", flush=True)
+        # Submit all tool_outputs at the same time
+        # print(tool_outputs)
+        self.submit_tool_outputs(tool_outputs, run_id)
+
+    def submit_tool_outputs(self, tool_outputs, run_id):
+        # Use the submit_tool_outputs_stream helper
+        with client.beta.threads.runs.submit_tool_outputs_stream(
+            thread_id=self.current_run.thread_id,
+            run_id=self.current_run.id,
+            tool_outputs=tool_outputs,
+            event_handler=EventHandler(),
+        ) as stream:
+            for text in stream.text_deltas:
+                print(text, end="", flush=True)
+            print()
 
 
 class ConradAssitant:
-    assistant_id = os.environ["ASSISTANT_V2"]
+    assistant_id = os.environ["ASSISTANT_V1"]
     thread_id = os.environ["THREAD_ID"]
 
     def __init__(self):
@@ -128,43 +164,22 @@ class ConradAssitant:
 
 def main():
     ca = ConradAssitant()
-    ca.create_assistant(
-        name="Conrad Assistant - Saim",
-        instructions='You are Conrad Labs assistant to help employees with the queries regarding Conrad Labs.\
-                    --- Response Instructions ---\
-                    1- You should always answer in two sentences max. \
-                    2- Use a paragraph format to answer.\
-                    3- Be precise in your answer.\
-                    4- Do not contain any asterisk or inverted commas (single or double) or quotations in your response.\
-                    5- If you do not understand the user\'s query, respond "Sorry, I am unable to answer your query, please clarify your query with regard to Conrad Labs"\
-                    6- If a user asks a query that does not relate to Conrad Labs, respond "Sorry, I am unable to answer your query, please clarify your query with regard to Conrad Labs"',
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_song_title",
-                    "description": "Returns a superhero name based on the integer parameter 'songnum'",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "songnum": {
-                                "type": "integer",
-                                "description": "An integer for indexing",
-                            },
-                        },
-                        "required": ["songnum"],
-                    },
-                },
-            }
-        ],
-    )
 
+    # ca.create_assistant(
+    #     name="Conrad Assistant - Saim",
+    #     instructions='You are Conrad Labs assistant to help employees with the queries regarding Conrad Labs.\n\
+    #       --- Response Instructions ---\n\
+    #         1- You should always answer in two sentences max.\n\
+    #           2- Use a paragraph format to answer.\n\
+    #             3- Be precise in your answer.\n\
+    #               5- If you do not understand the user\'s query, respond "Sorry, I am unable to answer your query, please clarify your query with regard to Conrad Labs"\n\
+    #                 6- If a user asks a query that does not relate to Conrad Labs, respond "Sorry, I am unable to answer your query, please clarify your query with regard to Conrad Labs"',
     #     tools=[
     #         {
     #             "type": "function",
     #             "function": {
     #                 "name": "get_song_title",
-    #                 "description": "Returns a superhero name based on the integer parameter 'songnum'",
+    #                 "description": "Returns a song name based on the integer parameter 'songnum'",
     #                 "parameters": {
     #                     "type": "object",
     #                     "properties": {
@@ -181,10 +196,9 @@ def main():
     # )
     # ca.create_thread()
 
-    # while True:
-    #     text = input("\nEnter your prompt: ")
-
-    #     ca.add_message_to_thread(role="user", content=text)
+    # client.beta.threads.runs.cancel(
+    #     "run_0Df0IDlNBpjnp0P6fUsU23R8", thread_id=ca.thread_id
+    # )
 
     text = input("\nEnter your prompt: ")
     ca.add_message_to_thread(role="user", content=text)
